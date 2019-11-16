38338e8b6dac64cb66110e5685afb476f34c7d97
maxueli <2601844278@qq.com>
2019-11-16 22:49:30 +0800

    xianzai

---
+++ xue/src/li/Li.java
@@ -0,0 +1,10 @@
+package li;
+
+public class Li {
+    public static void main (String [] args){
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+    }
+}
